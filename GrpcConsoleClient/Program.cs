02f97d72f805469ed1cd249f80aa6a293e25705b
Joseph <lai225joseph@hotmail.com>
2020-12-05 18:25:13 +0800

    gRPC

---
+++ GrpcConsoleClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using GrpcServicePractise;
+using System;
+using System.Threading.Tasks;
+using static GrpcServicePractise.Greeter;
+using static System.Console;
+
+namespace GrpcConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GreeterClient(channel);
+
+            var request = new HelloRequest
+            {
+                Name = "Joseph"
+            };
+            var response = await client.SayHelloAsync(request);
+            WriteLine(response.Message);
+            var yaResponse = await client.SayYaAsync(request);
+            WriteLine(yaResponse.Message);
+            ReadKey();
+        }
+    }
+}
